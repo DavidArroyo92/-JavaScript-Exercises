2a84280220b9f69e5ba39ddbae1ebc737f62efa7
DavidArroyo1992 <davidstream1992@gmail.com>
2020-10-25 15:09:57 -0300

    exercises 1. a,b...ready!

--- main.js
+++ main.js
@@ -1,17 +1,25 @@
-/*
-1) Variables y Operadores
-*/
+
+//1) Variables y Operadores
 
 /*
 Crear dos variables numéricas y utilizar el operador suma para 
 guardar el valor de la suma de ambos números en una 3er variable.
 */
+
 var Num1=5;
 var Num2=5;
 
 var Suma= Num2 + Num2;
 console.log(Suma);
 
+/*
+Crear dos variables de tipo String y 
+concatenarlas guardando el resultado en una 3er variable.
+*/
 
+var firstName='David ';
+var lastName='Arroyo';
 
+var User= firstName+lastName;
+console.log(User);
 
