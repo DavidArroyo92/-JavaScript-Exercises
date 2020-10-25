d1ec85c73727ef1d58cac087e3cf101bee3ebbbd
DavidArroyo1992 <davidstream1992@gmail.com>
2020-10-25 15:01:51 -0300

    initial commit

---
+++ main.js
@@ -0,0 +1,17 @@
+/*
+1) Variables y Operadores
+*/
+
+/*
+Crear dos variables numéricas y utilizar el operador suma para 
+guardar el valor de la suma de ambos números en una 3er variable.
+*/
+var Num1=5;
+var Num2=5;
+
+var Suma= Num2 + Num2;
+console.log(Suma);
+
+
+
+
